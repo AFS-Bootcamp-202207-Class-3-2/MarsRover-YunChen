d9ed16f579ad19d1c1052505b067b02625d38f4b
YunChen <1183728554@qq.com>
2022-07-21 17:40:10 +0800

    test:init direct must valid.

--- src/main/java/com/afs/enities/MarsRover.java
+++ src/main/java/com/afs/enities/MarsRover.java
@@ -4,6 +4,12 @@ package com.afs.enities;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Getter
 @Setter
@@ -24,7 +30,9 @@ public class MarsRover {
     private Location location;
     public MarsRover(int x,int y,String direct) throws Exception {
         location = new Location(x, y);
-        if (direct.length() != 1 ||
+        //初始化的方向只能是一个有效的方向
+        if (StringUtils.isEmpty(direct) ||
+                direct.length() != 1 ||
                 !(direct.equals(NORTH) ||
                         direct.equals(EAST) ||
                         direct.equals(SOUTH) ||
@@ -32,6 +40,19 @@ public class MarsRover {
             throw new Exception("Param direction error!");
         }
         currDirect = direct;
+    }
+    /**
+     * 验证单个命令是否能有效执行
+     * @param order
+     * @return
+     */
+    private boolean validSingleOrder(String order) {
+        return StringUtils.isEmpty(order) &&
+                order.length() == 1 &&
+                (order.equals(NORTH) ||
+                        order.equals(EAST) ||
+                        order.equals(SOUTH) ||
+                        order.equals(WEST));
     }
 
     /**
@@ -43,11 +64,50 @@ public class MarsRover {
 
     }
 
+    private String parseOrder(String order) {
+        if (!StringUtils.isEmpty(order) && order.length() == 1) {
+            singleOrder(order);
+        }
+        order = order.trim();
+        order = order.toUpperCase();
+        List<String> stringList = new ArrayList<>();
+        char[] chars = order.toCharArray();
+        return "";
+    }
+
     public int getLocationX() {
         return location.x;
     }
     public int getLocationY() {
         return location.x;
+    }
+
+    private void singleOrder(String order) {
+
+    }
+
+    private void batchOrder(String order) {
+
+    }
+
+    public void executeOrder(String order) {
+
+    }
+
+    public void update(String order) {
+
+    }
+
+    public void turnLeft() {
+
+    }
+
+    public void turnRight() {
+
+    }
+
+    public void move() {
+
     }
 
 }
--- src/test/java/com/afs/tdd/ApplicationTest.java
+++ src/test/java/com/afs/tdd/ApplicationTest.java
@@ -1,10 +1,26 @@
 package com.afs.tdd;
 
 import com.afs.enities.MarsRover;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import org.assertj.core.api.Fail;
 class ApplicationTest {
+    @Test
+    void should_init_right_direct_when_init_mars_rover_given_direct_A() {
+        //given
+        try {
+            MarsRover marsRover = new MarsRover(0, 0, "A");
+            Fail.fail("Param direction error!");
+        } catch (Exception e) {
+            Assertions.assertThat(e).hasMessageContaining("Param direction error!");
+        }
+        //when
+
+        //then
+    }
+
     @Test
     void should_Local_0_1_when_Order_M_given_M()throws Exception {
         //given
@@ -18,6 +34,8 @@ class ApplicationTest {
         assertThat(marsRover.getLocationX()).isEqualTo(exceptX);
         assertThat(marsRover.getLocationY()).isEqualTo(exceptY);
         assertThat(marsRover.getCurrDirect()).isEqualTo(exceptDirect);
+
+
     }
 
     
